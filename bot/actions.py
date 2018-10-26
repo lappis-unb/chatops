660a0138aa3e3d0479ef25f2bad0e18f5157a4b6
ArthurTemporim <arthurrtl@gmail.com>
2018-10-26 20:02:33 -0300

    Improve custom action and Makefile.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- bot/actions.py
+++ bot/actions.py
@@ -2,7 +2,7 @@ import json
 import requests
 import time
 
-from rasa_core.actions import Action
+from rasa_core_sdk import Action
 from rasa_core.events import SlotSet
 from rocketchat_py_sdk.driver import Driver
 
@@ -12,44 +12,47 @@ rocket_url = 'localhost:3000'
 header = None
 
 class My_bot(object):
-	hello = ''
-	is_online = False
-	livechat_data = None
+    hello = ''
+    is_online = False
+    livechat_data = None
 def verify_logged(bot, tais):
 
-	def get_data(error, data):
-		if error:
-			print((50*'=') + ' ERRO ' + (50 * '='))
-			return
-		tais.livechat_data = data
-		tais.is_online = data['online']
+    def get_data(error, data):
+        if error:
+            print((50*'=') + ' ERRO ' + (50 * '='))
+            return
+        tais.livechat_data = data
+        tais.is_online = data['online']
 
-	bot.call('livechat:getInitialData',[bot._login_token], get_data)
+    bot.call('livechat:getInitialData',[bot._login_token], get_data)
 
 def start(bot, tais):
-	bot.connect()
-	bot.login(user=bot_username, password=bot_password)
-	bot.subscribe_to_messages()
-	verify_logged(bot, tais)
-	time.sleep(1)
-	print('----------> Tais logou?')
-	print('----------> ' +str(tais.is_online))
-	print(tais.livechat_data)
-	if tais.is_online:
-		message = 'Tais esta online'
-	else:
-		message = 'Tais esta offline'
-	return message
+    try:
+        bot.connect()
+        bot.login(user=bot_username, password=bot_password)
+        bot.subscribe_to_messages()
+        verify_logged(bot, tais)
+        time.sleep(1)
+        print('----------> Tais logou?')
+        print('----------> ' +str(tais.is_online))
+        print(tais.livechat_data)
+        if tais.is_online:
+            message = 'Tais esta online'
+        else:
+            message = 'Tais esta offline'
+    except ConnectionRefusedError:
+            message = 'Desculpe, não consegui me conectar =/'
+
+    return message
 
 class ActionTaisOn(Action):
 
-	def name(self):
-		return 'action_tais_on'
+    def name(self):
+        return 'action_tais_on'
 
-	def run(self, dispatcher, tracker, domain):
-		tais = My_bot()
-		tais.hello = 'Ola meu nome e Tais'
-		message = start(Driver(url=rocket_url, ssl=False, debug=True), tais)
+    def run(self, dispatcher, tracker, domain):
+        tais = My_bot()
+        tais.hello = 'Ola meu nome e Tais'
+        message = start(Driver(url=rocket_url, ssl=False, debug=True), tais)
 
-		dispatcher.utter_message(message)
-                return []
+        dispatcher.utter_message(message)
